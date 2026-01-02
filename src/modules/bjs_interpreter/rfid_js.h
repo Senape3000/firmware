5d8108423e036cbb824d92620a9ee1bb57747cf3
Senape3000 <80119773+Senape3000@users.noreply.github.com>
2026-01-02 18:14:53 +0100

    Add RFID JavaScript header file

---
+++ src/modules/bjs_interpreter/rfid_js.h
@@ -0,0 +1,18 @@
+// Author: Senape3000
+// More info: https://github.com/Senape3000/firmware/blob/main/docs_custom/JS_RFID/RFID_API_README.md
+
+#if !defined(LITE_VERSION) && !defined(DISABLE_INTERPRETER)
+
+#ifndef __RFID_JS_H__
+#define __RFID_JS_H__
+
+#include <duktape.h>
+
+duk_ret_t putPropRFIDFunctions(duk_context *ctx, duk_idx_t obj_idx, uint8_t magic);
+duk_ret_t registerRFID(duk_context *ctx);
+
+duk_ret_t native_rfidRead(duk_context *ctx);
+duk_ret_t native_rfidReadUID(duk_context *ctx);
+
+#endif
+#endif
